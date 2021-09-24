4f2c7dfcf549494ac927018f8d6e9c8e5c5c3019
r-silver1 <r.elliot.silver@gmail.com>
2021-09-24 16:30:23 -0700

    added basic cube viewer

--- threejs-ng-snake/src/app/canvas-comp/canvas-comp.component.ts
+++ threejs-ng-snake/src/app/canvas-comp/canvas-comp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-canvas-comp',
@@ -10,7 +11,26 @@ export class CanvasCompComponent implements OnInit {
 
   constructor() { }
 
+  scene = new THREE.Scene()
+  geometry = new THREE.BoxGeometry(1,1,1)
+  mesh = new THREE.Mesh(this.geometry)
+
+  camera = new THREE.PerspectiveCamera(60, 800/600)
+
+
+  renderer: any;
+
   ngOnInit(): void {
+    this.scene.add(this.mesh)
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: <HTMLCanvasElement>document.querySelector('canvas.draw')
+    })
+    this.camera.position.z = 3
+    this.camera.position.x = .5
+    this.camera.position.y = 1
+    this.scene.add(this.camera)
+    this.renderer.setSize(800, 600)
+    this.renderer.render(this.scene, this.camera)
   }
 
 }
