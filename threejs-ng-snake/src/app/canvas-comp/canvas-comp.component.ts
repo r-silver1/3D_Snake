49a0e3cad3be5e4cd4a58be1a84af51b8a7c51f7
r-silver1 <r.elliot.silver@gmail.com>
2021-09-30 14:18:44 -0700

    added some changes

--- threejs-ng-snake/src/app/canvas-comp/canvas-comp.component.ts
+++ threejs-ng-snake/src/app/canvas-comp/canvas-comp.component.ts
@@ -8,29 +8,40 @@ import * as THREE from 'three';
   ]
 })
 export class CanvasCompComponent implements OnInit {
-
   constructor() { }
 
   scene = new THREE.Scene()
-  geometry = new THREE.BoxGeometry(1,1,1)
+  geometry = new THREE.BoxGeometry(1, 1, 1)
   mesh = new THREE.Mesh(this.geometry)
 
-  camera = new THREE.PerspectiveCamera(60, 800/600)
+  camera = new THREE.PerspectiveCamera(60, 800 / 600)
 
 
   renderer: any;
 
+  window_size(): void {
+    // @ts-ignore
+    const HEIGHT = document.getElementById('mainCanvas').clientHeight;
+    // @ts-ignore
+    const WIDTH = document.getElementById('mainCanvas').clientWidth;
+    this.renderer.setSize(WIDTH, HEIGHT);
+    this.camera.aspect = WIDTH / HEIGHT;
+    this.camera.updateProjectionMatrix();
+  }
+
   ngOnInit(): void {
     this.scene.add(this.mesh)
     this.renderer = new THREE.WebGLRenderer({
-      canvas: <HTMLCanvasElement>document.querySelector('canvas.draw')
+      canvas: document.querySelector('canvas.draw') as HTMLCanvasElement
+      // canvas: <HTMLCanvasElement> document.querySelector('canvas.draw') (bad form)
     })
     this.camera.position.z = 3
     this.camera.position.x = .5
     this.camera.position.y = 1
     this.scene.add(this.camera)
-    this.renderer.setSize(800, 600)
-    this.renderer.render(this.scene, this.camera)
+    // this.renderer.setSize(800, 600)
+    this.window_size();
+    this.renderer.render(this.scene, this.camera);
   }
 
 }
