b2adea248a91d60fd075e3bea48cd690e6c24506
r-silver1 <r.elliot.silver@gmail.com>
2021-10-02 17:46:47 -0700

    initial animation loop running

--- threejs-ng-snake/src/app/canvas-comp/canvas-comp.component.ts
+++ threejs-ng-snake/src/app/canvas-comp/canvas-comp.component.ts
@@ -7,19 +7,37 @@ import * as THREE from 'three';
   styles: [
   ]
 })
+
+
 export class CanvasCompComponent implements OnInit {
-  constructor() { }
 
-  scene = new THREE.Scene()
-  geometry = new THREE.BoxGeometry(1, 1, 1)
-  mesh = new THREE.Mesh(this.geometry)
+  public scene: THREE.Scene;
+  public geometry: THREE.BoxGeometry;
+  public mesh: THREE.Mesh;
+  public camera: THREE.PerspectiveCamera;
+  public renderer: any;
+  public start: any;
 
-  camera = new THREE.PerspectiveCamera(60, 800 / 600)
+  constructor() {
+
+    this.scene = new THREE.Scene();
+    this.geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.mesh = new THREE.Mesh(this.geometry);
+    this.camera = new THREE.PerspectiveCamera(60, 800 / 600);
+    this.start = -1;
+    this.animate = this.animate.bind(this);
+  }
 
 
-  renderer: any;
 
-  window_size(): void {
+  init_cameras(): void {
+    this.camera.position.z = 3;
+    this.camera.position.x = .5;
+    this.camera.position.y = 1;
+    this.scene.add(this.camera);
+  }
+
+  window_set_size(): void {
     // @ts-ignore
     const HEIGHT = document.getElementById('mainCanvas').clientHeight;
     // @ts-ignore
@@ -29,19 +47,38 @@ export class CanvasCompComponent implements OnInit {
     this.camera.updateProjectionMatrix();
   }
 
+  window_size_listener(): void {
+    window.addEventListener('resize', () => {
+      this.window_set_size();
+    });
+  }
+
+  render_all(): void {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  // @ts-ignore
+  animate(timestamp): FrameRequestCallback {
+    if (this.start === -1){
+      this.start = timestamp;
+    }
+    const elapsed = timestamp - this.start;
+    // console.log('elapsed:' + elapsed);
+    this.render_all()
+    requestAnimationFrame(this.animate);
+  }
+
   ngOnInit(): void {
-    this.scene.add(this.mesh)
+    this.scene.add(this.mesh);
     this.renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('canvas.draw') as HTMLCanvasElement
       // canvas: <HTMLCanvasElement> document.querySelector('canvas.draw') (bad form)
-    })
-    this.camera.position.z = 3
-    this.camera.position.x = .5
-    this.camera.position.y = 1
-    this.scene.add(this.camera)
-    // this.renderer.setSize(800, 600)
-    this.window_size();
-    this.renderer.render(this.scene, this.camera);
+    });
+    this.init_cameras();
+    this.window_set_size();
+    this.window_size_listener();
+    requestAnimationFrame(this.animate);
+    // this.renderer.render(this.scene, this.camera);
   }
 
 }
