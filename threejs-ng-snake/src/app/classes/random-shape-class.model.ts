cadfd1b755986815c80dfcdf854ee796b1a2c8d2
r-silver1 <r.elliot.silver@gmail.com>
2021-12-15 23:26:08 -0800

    not quite the shape I was looking for, more of a paper crane, will work more later

--- threejs-ng-snake/src/app/classes/random-shape-class.model.ts
+++ threejs-ng-snake/src/app/classes/random-shape-class.model.ts
@@ -16,7 +16,96 @@ export class RandomShapeClass {
             this.radius = radius;
             this.position = position;
 //             this.geometry = new THREE.BoxGeometry(radius, radius, radius)
-            this.geometry = this.makeGeometry(radius)
+//             this.geometry = this.makeGeometry(radius)
+            //temp
+            let maxPoints = 5
+            this.geometry = this.makeRandomGeometry(maxPoints, radius)
+    }
+
+
+    // https://sites.math.washington.edu/~king/coursedir/m445w04/notes/vector/coord.html
+//     makeCircle(index:number, maxPoints:number, radius:number, yIndex:number) : Float32Array[] {
+    makeCircle(index:number, maxPoints:number, radius:number, yIndex:number) : number[] {
+        // 0 <= index < maxPoints - 1
+        const pointsNum: number = maxPoints - index
+        const circleRadius: number = radius/(index+1)
+//         let circlePointsMat: Float32Array[] = [];
+        let circlePointsMat: Array<any> = []
+        let last: number = 0.0
+        let thetaDiff: number = 360.0/maxPoints
+        for (let i = 0; i < maxPoints; i++) {
+            // random value between last generated angle and theta increment
+            let theta:number = last + (Math.random() * (thetaDiff))
+            last += thetaDiff
+            let iX:number = Math.cos(theta) * circleRadius;
+            let iZ:number = Math.sin(theta) * circleRadius;
+//             circlePointsMat.push(new Float32Array([iX, yIndex, iZ]))
+            circlePointsMat.push([iX, yIndex, iZ])
+
+        }
+        return circlePointsMat;
+    }
+
+//     makeCirclesArrays(maxPoints: number, radius:number) : THREE.BufferGeometry {
+    makeCirclesArrays(maxPoints: number, radius:number) : Array<any> {
+        let numCircles = maxPoints - 1;
+        let yStep = radius / numCircles;
+//         let index = 0;
+        let yIndex = 0;
+        let circles = new Array(numCircles)
+        for (let i = 0; i < numCircles; i++) {
+            let currCircle = this.makeCircle(i, maxPoints, radius, yIndex)
+//             circles.push(currCircle)
+            circles[i] = currCircle
+            yIndex += yStep;
+            maxPoints -= 1;
+        }
+    //         return new THREE.BufferGeometry()
+        return circles;
+
+    }
+
+// //     generateBufferFromArr()
+//     pushPoint(arrPoints: Array<any>, bufferGeo: number[]) : void {
+//         bufferGeo.concat()
+//     }
+
+    pushTwoCircles(circleOne: Array<any>, circleTwo: Array<any>, bufferArr: number[]) : number[] {
+        let indxOne = 0;
+        let indxTwo = 0;
+        let indxBool = true;
+        //circle1 should be longer circle; circle2 should have one less entry
+        const numPoints = circleOne.length;
+        while(indxOne < numPoints-1){
+            if(indxBool){
+                bufferArr = bufferArr.concat(circleOne[indxOne]);
+                bufferArr = bufferArr.concat(circleTwo[indxTwo]);
+                indxOne++;
+                bufferArr = bufferArr.concat(circleOne[indxOne]);
+            }else{
+                bufferArr = bufferArr.concat(circleTwo[indxTwo]);
+                bufferArr = bufferArr.concat(circleOne[indxOne]);
+                indxTwo++;
+                bufferArr = bufferArr.concat(circleTwo[indxTwo]);
+            }
+            indxBool = !indxBool
+        }
+        return bufferArr
+    }
+
+    makeRandomGeometry(maxPoints: number, radius: number) : THREE.BufferGeometry {
+        let circlesPointsArr = this.makeCirclesArrays(maxPoints, radius);
+//         let currObjArr = new Float32Array()
+        let currObjArr:number[] = []
+        for(let i = 0; i < circlesPointsArr.length-1; i++){
+//             console.log(circlesPointsArr)
+            currObjArr = this.pushTwoCircles(circlesPointsArr[i], circlesPointsArr[i+1], currObjArr)
+        }
+        let geometry = new THREE.BufferGeometry();
+//         console.log(currObjArr)
+        geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(currObjArr), 3));
+        geometry.computeVertexNormals();
+        return geometry
     }
 
     // https://threejs.org/docs/index.html#api/en/core/BufferGeometry.groups
